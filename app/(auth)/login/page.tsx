4e6b1dfd0fec19c61764c92bb5acc5d62e422550
AbdulMoeez231 <moeezbuttab231@gmail.com>
2025-08-12 16:33:48 +0500

    Update dependencies and enhance styling with Tailwind CSS and animations. Integrate toast notifications and improve layout structure in the main application files.

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { LoginForm } from '@/components/auth/LoginForm'
+
+const Loginpage = () => {
+    return (
+        <div className='flex h-svh items-center'>
+            <LoginForm />
+        </div>
+    )
+}
+
+export default Loginpage
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import LoginButton from "@/components/auth/LoginLogoutButton";
+import UserGreetText from "@/components/home/UserGreetText";
 
-const page = () => {
+export default function Home() {
   return (
-    <div>page</div>
-  )
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <UserGreetText />
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
+          <LoginButton />
+        </div>
+      </div>
+    </main>
+  );
 }
-
-export default page
