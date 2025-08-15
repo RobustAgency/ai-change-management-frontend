f2f8a93f159c22307d62d235510d6aab02835a43
AbdulMoeez231 <moeezbuttab231@gmail.com>
2025-08-15 16:28:06 +0500

    Adjust MainLayout padding for improved layout consistency on different screen sizes.

--- layouts/MainLayout.tsx
+++ layouts/MainLayout.tsx
@@ -44,7 +44,7 @@ export default function MainLayout({ children }: MainLayoutProps) {
                         <Sidebar collapsed={desktopCollapsed} onNavigate={() => { }} />
                     </div>
                 </aside>
-                <main className="min-h-[calc(100vh-3.5rem)] p-6">
+                <main className="min-h-[calc(100vh-3.5rem)] p-4 md:p-6">
                     {children}
                 </main>
             </div>
