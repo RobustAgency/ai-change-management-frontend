48d80e91194c13b6725d59a33ed535ad85c29d87
AbdulMoeez231 <moeezbuttab231@gmail.com>
2025-08-22 10:09:31 +0500

    Remove unused Badge import from Greetings component to streamline code

--- components/app/dashboard/Greetings.tsx
+++ components/app/dashboard/Greetings.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useAuth } from '@/providers/AuthProvider'
-import { Badge } from '@/components/ui/badge'
 
 const Greetings = () => {
     const { profile } = useAuth()
